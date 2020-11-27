d222595af0c16640a2d12f0a787a62820a9e77a9
Aminigbo <61628021+Aminigbo@users.noreply.github.com>
2020-11-27 17:17:30 +0100

    changes

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-   <title>Tik8 - No.1 ticketing platform</title>
+   <title>Oga Jobs</title>
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <meta content="" name="keywords">
   <meta content="" name="description">
