7d4ce19d54d0d579844ef76078191ae21b70f412
Aminigbo <61628021+Aminigbo@users.noreply.github.com>
2020-11-27 15:56:43 +0100

    changes

---
+++ index.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+   <title>Tik8 - No.1 ticketing platform</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+ <!-- Favicons -->
+ <link href="img/logo-tik.jpg" rel="icon">
+ <link href="img/logo-tik.jpg" rel="apple-touch-icon">
+ 
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet"> 
+  <link href="assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+
+
+ <style type="text/css">
+</style>
+
+
+</head>
+
+<body>
+
+
+  <section id="section1" class="section1 header">
+    <img src="assets/vectors/1.png" class="vector1 wow fadeInDown">
+
+    <a href="" class="about wow fadeInUp header-btns">About Us</a>
+    <a href="" class="find wow fadeInUp header-btns">Find Job</a>
+    <a href="" class="blog wow fadeInUp header-btns">Blog</a>
+
+    <a href="" class="post-job wow fadeInUp">Post a Job</a>
+    <a href="" class="join-us wow fadeInUp">Join Us</a>
+
+    <div class="dream-jobs wow fadeInLeft">Dream <span class="color">Jobs</span>. Top <span class="color">Talent</span>. 
+    All in <span class="color">One</span> <span class="color">Place</span>.</div>
+
+    <div class="preamble wow fadeInUp">
+    Ready to post a job? Get started with Indeed, where you can post an open role in
+     minutes. Jobs you pay to post on Indeed will also appear on Glassdoor.
+    </div>
+
+    <img src="assets/vectors/2.png" class="vector2 wow fadeInDown">
+
+    <div class="two_btnss">
+      <a href="" class="next_job wow fadeInUp">Find Your Next Job</a>
+
+      <a href="" class="hire_talent wow fadeInUp">Hire Talent</a>
+    </div>
+    
+    
+  </section>
+  
+    <div class="search_title wow fadeInUp">
+      Search for Your Dream job
+    </div>
+
+    <input type="text" name="" id="" class="search-form wow fadeInUp" placeholder="Search Job Title">
+
+    <h class="dpt-header wow fadeInUp">
+    Choose Department
+    </h>
+    
+    <h class="dpt-header-title wow fadeInUp">
+    The choice is yours, we’ve got you covered
+    </h>
+
+
+    <div class="dpt-slider wow fadeInUp">
+
+    <div class="owl-carousel owl-theme">
+    <div class="item"><h4>1</h4></div>
+    <div class="item"><h4>2</h4></div>
+    <div class="item"><h4>3</h4></div>
+    <div class="item"><h4>4</h4></div>
+    <div class="item"><h4>5</h4></div>
+    <div class="item"><h4>6</h4></div>
+    <div class="item"><h4>7</h4></div>
+    <div class="item"><h4>8</h4></div>
+    <div class="item"><h4>9</h4></div>
+    <div class="item"><h4>10</h4></div>
+    <div class="item"><h4>11</h4></div>
+    <div class="item"><h4>12</h4></div>
+</div>
+
+
+    </div>
+
+    <div class="more wow fadeInUp">
+        More from Job finder
+    </div>
+
+    <img src="assets/vectors/sale.png" class="blog1" alt="">
+    <img src="assets/vectors/sale.png" class="blog2" alt="">
+    <img src="assets/vectors/sale.png" class="blog3" alt="">
+    <img src="assets/vectors/sale.png" class="blog4" alt="">
+
+    <div class="footer">
+    <img src="assets/vectors/footer.png" class="footer-vector wow fadeInDown">
+
+    <br><br>
+    
+    <div class="who-we-are wow fadeInDown">
+      <b>Who We Are</b> 
+
+      <b class="social">
+      <img src="assets/vectors/ig.png" class="social-icons wow fadeInDown">
+      <img src="assets/vectors/fb.png" class="social-icons wow fadeInDown">
+      <img src="assets/vectors/twitter.png" class="social-icons wow fadeInDown">
+      </b>
+      
+      <br><br>
+      <p class="wow fadeInDown">
+        Finding highly skilled employees can be time consuming and costly. And trying to find 
+        a new job can be a frustrating experience. Find out how we can help you with your hiring 
+        and job search needs.Finding highly skilled employees can be time consuming and costly.
+        And trying to find a new job can be a frustrating experience. Find out how we can help
+        you with your hiring and job search needs.Finding highly skilled employees can be time
+        consuming and costly. And trying to find a new job can be a frustrating experience. 
+        Find out how we can help you with your hiring and job search needs.Finding highly
+        skilled employees can be time consuming and costly. And trying to find a new job can
+        be a frustrating experience. Find out how we can help you with your hiring and job
+        search needs.
+      </p>
+    </div>
+    </div>
+
+    <div class="by">
+      <a href="https://hthub.com.ng/" class="by-a">
+      Built with  <i class="fa fa-heart" style="color:red;"></i> by HARVOXX TECH. HUB
+      </a>
+    </div>
+
+  <!-- <a href="#" class="back-to-top"><i class="fa fa-angle-up"></i></a> -->
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script> 
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script> 
+  <script src="assets/vendor/wow/wow.min.js"></script> 
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/superfish/superfish.min.js"></script> 
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+<script type="text/javascript">
+  $(document).ready(function(){
+    // alert("")
+  })
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+    
+</script>
+
+<!-- <script src="//code.tidio.co/4hx5f11qnh54qrylyuwlvjupscrxgh2u.js" async></script> -->
+
+</body>
+
+</html>
