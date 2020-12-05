e688cd5fa42fa7555406f4889640bc7ed1e0fbc6
Aminigbo <61628021+Aminigbo@users.noreply.github.com>
2020-12-05 15:04:25 +0100

    changes

--- description.php
+++ description.php
@@ -6,7 +6,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <meta name="theme-color" content=" #005B49" />
-   <title>Oga Jobs</title>
+   <title>Job Padi</title>
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <meta content="" name="keywords">
   <meta content="" name="description">
